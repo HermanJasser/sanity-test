e923e70859b5d8633e677ea2a6e19f0f6130f649
HermanJasser <herjas00989@stud.noroff.no>
2024-03-08 09:52:14 +0100

    til inge

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let cont = document.querySelector(".cont");
 
 //console.log("hei")
-async function listBeer(container) {
+async function getBeerApi(container) {
     try{
         const api = `https://yf3khxzc.api.sanity.io/v2022-03-07/data/query/production?query=*%5B_type%3D%3D+%27beer%27%5D%7B%0A++title%2C+price%0A%7D`;
         //console.log(api);
@@ -13,11 +13,22 @@ async function listBeer(container) {
         console.log(data);
         beerApi = data.result;
        console.log(beerApi);
-
+        listBeer(beerApi, container);
     } catch (error){
         console.error("Error message: " + error)
         container.innerHTML = `<p>Kan ikke finne noen produkter</p>`
     }
 }
 
-listBeer(cont);
+getBeerApi(cont);
+
+
+function listBeer(api, output) {
+    output.innerHTML = "";
+    let beerContainer = "";
+    for (let beer of api) {
+      beerContainer += `<div class="beer"><p class="beer-title">${beer.title}</p>
+      <p>${beer.price} kr</p></div>`;
+    }
+    output.innerHTML = beerContainer;
+  }
